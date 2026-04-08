9379e28070181207ab8a8caf52536978fb7f5365
= <=>
2026-04-08 17:46:03 +0200

    up

--- Lezioni_Python/Lezione2_Statement/esempi_IF.py
+++ Lezioni_Python/Lezione2_Statement/esempi_IF.py
@@ -4,4 +4,6 @@
 
 #Es3. Dato un certo numero verifica se si trova tra 20 e 100 oppure se si trova tra 50 e 200
 
-#Es4. Gioco dei dadi. Lancia 2 dadi (numero casuale tra 1 e un numero scelto dall'utente. Faccio scegliere all'utente il numero di facce del dado). Se i due numeri sono uguali l'utente vince, altrimenti perde.
+#Es4. Numeri pari e dispari. Verifica se un numero fornito da un utente è pari o dispari
+
+#Es. Gioco dei dadi. Lancia 2 dadi (numero casuale tra 1 e un numero scelto dall'utente. Faccio scegliere all'utente il numero di facce del dado). Se i due numeri sono uguali l'utente vince, altrimenti perde.
