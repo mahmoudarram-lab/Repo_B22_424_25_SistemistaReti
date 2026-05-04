26d2c9ba8a900327cdf3d912dfd84635f8415915
jovanymfoutou <jovanymfoutou@gmail.com>
2026-05-04 16:00:52 +0200

    Update 04_funzioni.py

--- Lezioni_Python/Lezione3_Funzioni/04_funzioni.py
+++ Lezioni_Python/Lezione3_Funzioni/04_funzioni.py
@@ -1,6 +1,21 @@
+import random
+
+
 #Dichiaro la funzione 
 def benvenuto():
     print("ciao Dario")
-    numerocasuale = math.
+    numerocasuale = random.randint(0, 101)
+    print(f"Oggi il tuo numero è il {numerocasuale}")
 
 #eseguo la funzione 
+benvenuto()
+
+
+# Le funzioni possono avere dei parametri in ingresso 
+def descrivi_server(nome, ip):
+    print(f"server: {nome} - IP: {ip}")
+
+
+descrivi_server("Webserver", "192.168.1.20")
+descrivi_server("DBServer", "192.168.1.10")
+descrivi_server("BEServer", "192.168.1.25")
